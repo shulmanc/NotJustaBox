42e0d2820e19a3b1439aab8c924f430594f7ff42
Cole Shulman <60298572+shulmanc@users.noreply.github.com>
2020-04-26 11:20:37 -0400

    Delete databasehandler.php

--- NotJustABoxPHP/databasehandler.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "password123";
-$dbName = "NotJustaBox";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
