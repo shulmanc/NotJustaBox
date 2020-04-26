77ba760220ad946d7b5db00ff132d3d5b8cc74c7
Cole Shulman <60298572+shulmanc@users.noreply.github.com>
2020-04-25 20:28:33 -0400

    Add files via upload

---
+++ NotJustABoxPHP/databasehandler.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "password123";
+$dbName = "NotJustaBox";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
